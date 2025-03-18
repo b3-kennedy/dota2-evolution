702ddb99839954a9f5e4bb5705c47ff60fcac365
b3-kennedy <Benjamin3.Kennedy@live.uwe.ac.uk>
2025-03-18 16:22:51 +0000

    buildings now cant be placed on water

--- game/scripts/vscripts/placement_check.lua
+++ game/scripts/vscripts/placement_check.lua
@@ -2,15 +2,19 @@ placement_check = class({})
 
 function placement_check:IsPlacementBlocked(player, position, caster, radius)
     local all_buildings = Entities:FindAllByClassname("npc_dota_tower")
+    
+    if position.z < 256 then
+        self:SendPlacementError(caster, "Cannot place building on water")
+        return true
+    end
+
+
     for _, building in pairs(all_buildings) do
         local building_pos = building:GetAbsOrigin()
         local distance_sqr = (position - building_pos):Length2D() -- Squared distance
-
-        print("Building found:", building:GetUnitName(), "DistanceSqr:", distance_sqr)
-
         -- Check if within blocking radius
         if distance_sqr < radius then
-            self:SendPlacementError(caster)
+            self:SendPlacementError(caster, "Cannot place building here - too close to another building")
             return true
         end
     end
@@ -18,10 +22,10 @@ function placement_check:IsPlacementBlocked(player, position, caster, radius)
     return false
 end
 
-function placement_check:SendPlacementError(caster)
+function placement_check:SendPlacementError(caster, msg)
     CustomGameEventManager:Send_ServerToPlayer(
         PlayerResource:GetPlayer(caster:GetPlayerOwnerID()), 
         "display_custom_error", 
-        { message = "Cannot place building here - too close to another unit" }
+        { message = msg }
     )
 end
